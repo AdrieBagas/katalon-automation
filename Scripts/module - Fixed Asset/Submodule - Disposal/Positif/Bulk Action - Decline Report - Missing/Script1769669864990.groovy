6397e56206816126dd9acc60051bfb86e80175d4
AdrieBagas <107867402+AdrieBagas@users.noreply.github.com>
2026-04-02 00:34:05 +0700

    Add files via upload

---
+++ Scripts/module - Fixed Asset/Submodule - Disposal/Positif/Bulk Action - Decline Report - Missing/Script1769669864990.groovy
@@ -0,0 +1,31 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.click(findTestObject('Object - Disposal/Badge - Missing'))
+
+CustomKeywords.'company.EntitySelector.selectCheckboxinTable'(assetName)
+
+WebUI.click(findTestObject('Object - Disposal/Button - Bulk Action'))
+
+WebUI.click(findTestObject('Object - Disposal/Elipsis List - Decline Report'))
+
+WebUI.click(findTestObject('Object - Disposal/Button - Decline'))
+
+WebUI.verifyElementText(findTestObject('Object - Disposal/Toast - Success'), 'Success, report has been declined.')
+
