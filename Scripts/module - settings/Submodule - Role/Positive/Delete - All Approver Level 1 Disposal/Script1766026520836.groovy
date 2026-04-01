bcd63761a2814107e07ea0928adbedefcd758268
AdrieBagas <107867402+AdrieBagas@users.noreply.github.com>
2026-04-02 00:21:29 +0700

    Add files via upload

---
+++ Scripts/module - settings/Submodule - Role/Positive/Delete - All Approver Level 1 Disposal/Script1766026520836.groovy
@@ -0,0 +1,42 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.click(findTestObject('Object - Settings - Role/Tab - Disposal'))
+
+'Always include the \'groupName\' variable to target the XPath.'
+WebUI.click(findTestObject('Object - Settings - Role/Button - Select Group', [('groupName') : 'Test Role']))
+
+WebUI.click(findTestObject('Object - Settings - Role/Button - Delete All Approver Level 1'))
+
+WebUI.verifyElementVisible(findTestObject('Object - Settings - Role/Confirmation Dialog - Remove User'))
+
+WebUI.click(findTestObject('Object - Settings - Role/Button - Remove in Dialog'))
+
+WebUI.verifyElementVisible(findTestObject('Object - Settings - Role/Toast - Success'))
+
+WebUI.verifyElementText(findTestObject('Object - Settings - Role/Toast - Success'), 'Success, level 1 approver has been removed.')
+
+WebUI.click(findTestObject('Object - Settings - Role/Button - Changelog Addon Role'))
+
+'Always include the \'action\' and \'userName\' variable to target the XPath.'
+WebUI.verifyElementVisible(findTestObject('Object - Settings - Role/Verify Data Table - Changelog', [('action') : 'Remove'
+            , ('userName') : '-']))
+
+WebUI.click(findTestObject('Object - Settings - Role/Button - Close Dialog'))
+
---
+++ Scripts/module - settings/Submodule - Role/Positive/Edit - Assign Role from Detail User Manager Transfer/Script1766376392226.groovy
@@ -0,0 +1,55 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+CustomKeywords.'company.SpecificElipsis.chooseSpesificUserName'(GlobalVariable.defaultSecondaryUserName)
+
+WebUI.click(findTestObject('Object - Settings - Role/Button - Detail From Elipsis'))
+
+WebUI.click(findTestObject('Object - Settings - Role/Button - Tab Transaction Role in Detail User'))
+
+WebUI.click(findTestObject('Object - Settings - Role/Button - Assign Role'))
+
+WebUI.click(findTestObject('Object - Settings - Role/Button - Select Group in Dialog'))
+
+CustomKeywords.'company.EntitySelector.selectGroupInDialog'(GlobalVariable.defaultGroupName)
+
+WebUI.click(findTestObject('Object - Settings - Role/Button - Select in Dialog'))
+
+WebUI.click(findTestObject('Object - Settings - Role/Dropdown - Select Transaction in Detail User'))
+
+WebUI.click(findTestObject('Object - Settings - Role/Dropdown - Option Transaction Role Transfer'))
+
+WebUI.click(findTestObject('Object - Settings - Role/Dropdown - Select Role in Detail User'))
+
+WebUI.click(findTestObject('Object - Settings - Role/Dropdown - Option Role in Detail User Manager'))
+
+WebUI.click(findTestObject('Object - Settings - Role/Button - Submit in Dialog'))
+
+WebUI.verifyElementVisible(findTestObject('Object - Settings - Role/Toast - Success'))
+
+WebUI.verifyElementText(findTestObject('Object - Settings - Role/Toast - Success'), 'Success, transaction role has been assigned.')
+
+WebUI.click(findTestObject('Object - Settings - Role/Button - Changelog Transaction Role in Detail User'))
+
+WebUI.verifyElementVisible(findTestObject('Object - Settings - Role/Verify Data Table - Changelog in Detail User', [('action') : 'Assign']))
+
+WebUI.click(findTestObject('Object - Settings - Role/Button - Close Dialog'))
+
+WebUI.back()
+
