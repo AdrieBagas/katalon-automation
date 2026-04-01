a13894cc655a43e0e7646458743c5d236ac6dac6
AdrieBagas <107867402+AdrieBagas@users.noreply.github.com>
2026-04-02 00:29:38 +0700

    Add files via upload

---
+++ Scripts/module - Fixed Asset/submodule - Assignment/Positive/Handover Asset - With all asset unselect from my asset/Script1766738455310.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+CustomKeywords.'company.CustomKeyword.loginToApp'(GlobalVariable.EmailManajer, GlobalVariable.Password)
+
+CustomKeywords.'company.UrlValidator.verifyUrl'('/my-asset/task/asset-assignment')
+
+WebUI.click(findTestObject('Object - Assignment/Button - Action'))
+
+WebUI.click(findTestObject('Object - Assignment/Action - Confirm Handover'))
+
+WebUI.uncheck(findTestObject('Object - Assignment/Checkbox - Asset to Assign'))
+
+WebUI.click(findTestObject('Object - Assignment/Button - Cancel Transaction'))
+
+WebUI.click(findTestObject('Object - Assignment/Button - Continue'))
+
+CustomKeywords.'company.UrlValidator.verifyUrl'('/asset/assignment/transaction')
+
+CustomKeywords.'company.AssetListValidator.verifySpecificRowData'([('status') : 'Canceled'], 0)
+
---
+++ Scripts/module - Fixed Asset/submodule - Assignment/Positive/Handover Asset - With asset non-tag/Script1766127605917.groovy
@@ -0,0 +1,73 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+CustomKeywords.'company.CustomKeyword.loginToApp'(GlobalVariable.EmailManajer, GlobalVariable.Password)
+
+CustomKeywords.'company.UrlValidator.verifyUrl'('/assets/assignment/transaction')
+
+WebUI.click(findTestObject('Object - Assignment/Button - Action'))
+
+WebUI.click(findTestObject('Object - Assignment/Action - Handover'))
+
+WebUI.delay(3)
+
+WebUI.click(findTestObject('Object - Assignment/Button - Next'))
+
+WebUI.delay(2)
+
+WebUI.click(findTestObject('login session object/Button - Profile'))
+
+WebUI.click(findTestObject('login session object/Action - Logout'))
+
+CustomKeywords.'company.CustomKeyword.loginToApp'(GlobalVariable.EmailBasic, GlobalVariable.Password)
+
+WebUI.click(findTestObject('login session object/Button - Profile'))
+
+WebUI.click(findTestObject('login session object/Action - My Asset'))
+
+WebUI.click(findTestObject('Object - Assignment/Button - Action'))
+
+WebUI.click(findTestObject('Object - Assignment/Action - Confirm Handover'))
+
+WebUI.check(findTestObject('Object - Assignment/Checkbox - Asset to Assign confirmation'))
+
+WebUI.click(findTestObject('Object - Borrow/Button - Confirm'))
+
+WebUI.click(findTestObject('Object - Borrow/Button - Continue'))
+
+WebUI.verifyElementText(findTestObject('Object - Borrow/Toast - Success'), 'Success, handover asset has been confirmed.')
+
+WebUI.click(findTestObject('login session object/Button - Profile'))
+
+WebUI.click(findTestObject('login session object/Action - Logout'))
+
+CustomKeywords.'company.CustomKeyword.loginToApp'(GlobalVariable.EmailManajer, GlobalVariable.Password)
+
+CustomKeywords.'company.UrlValidator.verifyUrl'('/assets/assignment/transaction')
+
+WebUI.click(findTestObject('Object - Assignment/Button - Action'))
+
+WebUI.click(findTestObject('Object - Assignment/Action - Handover'))
+
+WebUI.click(findTestObject('Object - Assignment/Button - Handover'))
+
+WebUI.verifyElementText(findTestObject('Object - Assignment/Toast - Success'), 'Success, asset has been handed over.')
+
+CustomKeywords.'company.AssetListValidator.verifySpecificRowData'([('status') : 'Assigned'], 0)
+
